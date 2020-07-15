4538d2b8d6820b1e1be3eccfd674e6338c7f7cb3
younessidbakkasse <std.youness@gmail.com>
2020-07-15 22:00:29 +0200

    finaly a working app

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function calculate() {
     var birthyear = document.getElementById("birthyear").value;
     var sleep = (2020 - birthyear) * 0.3;
+    var h3 = document.createElement('h3');
+    var answer = document.createTextNode("You've slept " + sleep.toFixed(0) + " years in your life.");
+    h3.appendChild(answer);
+    document.getElementById('results').appendChild(h3);
+    document.getElementById("birthyear").style.display = "none";
+    document.getElementById("calcule").style.display = "none";
+    document.getElementById("reset").style.display = "block";
+}
+
+function reset(){
+    document.getElementById('results').remove;
+    document.getElementById('birthyear').style.display = "block";
 }
