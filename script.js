e38c6aa7565af5416163a0c33f855a4ed21e83ac
younessidbakkasse <std.youness@gmail.com>
2020-07-16 10:21:12 +0200

    validating the form

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function calculate() {
     var birthyear = document.getElementById("birthyear").value;
-    var sleep = (2020 - birthyear) * 0.3;
+    var sleep = (2021 - birthyear) * 0.3;
+    if (birthyear > 2020 || birthyear < 0) {
+        alert("Please enter a valid birthyear");
+        return false;
+      }
     var h3 = document.createElement('h3');
     var answer = document.createTextNode("You've slept " + sleep.toFixed(0) + " years in your life.");
     h3.appendChild(answer);
