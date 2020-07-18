f337b0e1a666775422349a1be1044082d9f021df
younessidbakkasse <std.youness@gmail.com>
2020-07-18 21:12:02 +0200

    optimisong js file

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function calculate() {
-    var birthyear = document.getElementById("birthyear").value;
-    var sleep = (2021 - birthyear) * 0.3;
+    var birthYear = document.getElementById("birthyear").value;
+    var today = new Date();
+    var presentYear = today.getFullYear();
+    var age = (presentYear - birthYear) * 365;
+
+    //input validation 
     if (birthyear > 2020 || birthyear < 0) {
         alert("Please enter a valid birthyear");
         return false;
       }
-    var h3 = document.createElement('h3');
-    var answer = document.createTextNode("You've slept " + sleep.toFixed(0) + " years in your life.");
-    h3.appendChild(answer);
-    document.getElementById('results').appendChild(h3);
-    document.getElementById("birthyear").style.display = "none";
-    document.getElementById("calcule").style.display = "none";
+
+    var ele = document.getElementById("calcule");
+    ele.textContent = "You are " + age + " days old.";
 }
